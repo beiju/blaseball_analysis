e06d94a780941ad9c01bec18b1e87a37126dc84f
beiju <beiju@me.com>
2021-03-15 23:35:09 -0700

    counting hit types

--- main.py
+++ main.py
@@ -15,12 +15,10 @@ num_games = 99
 
 
 def download_games():
-    games = chronicler.get_games(season=13, team_ids=['8d87c468-699a-47a8-b40d-cfb73a5660ad'])
+    games = chronicler.get_games(season=10, team_ids=['8d87c468-699a-47a8-b40d-cfb73a5660ad'])
     for i, game in enumerate(games):
-        if i != 24:
-            continue
         game_url = f"https://api.sibr.dev/chronicler/v1/games/updates?game={game['gameId']}&count=1000"
-        game_file_path = f"data/{i}.json"
+        game_file_path = f"data-s10/{i}.json"
         urlretrieve(game_url, game_file_path)
         print("Downloaded game", i)
 
@@ -31,6 +29,33 @@ out_without_reaching_re = re.compile(
 out_at_base_re = re.compile(
     r'^(.+) (?:out at (?:first|second|third|fourth) base\.|gets caught stealing)|A murder of Crows ambush (.+)!$',
     flags=re.MULTILINE)
+
+
+crab_hits_onto_base = 0
+crab_hits_into_hr = 0
+crab_hits_into_outs = 0
+crab_hits_into_fc = 0
+crab_fouls = 0
+crab_walks = 0
+
+crab_strikeouts_swinging = 0
+crab_strikeouts_looking = 0
+
+opponent_hits_onto_base = 0
+opponent_hits_into_hr = 0
+opponent_hits_into_outs = 0
+opponent_hits_into_fc = 0
+opponent_fouls = 0
+opponent_walks = 0
+
+opponent_strikeouts_swinging = 0
+opponent_strikeouts_looking = 0
+
+def is_crab(update):
+    if update['topOfInning']:
+        return update['awayTeam'] != CLAB
+    else:
+        return update['homeTeam'] != CLAB
 
 
 class GameState(object):
@@ -50,6 +75,28 @@ class GameState(object):
         "is now Reverberating wildly!",
         "is Partying!",
         " has been cured of their peanut allergy!",
+        "BIRD NOISES",
+        "The Shame Pit activates!",
+        "These birds hate Blaseball!",
+        "Rogue Umpire incinerated ",
+        "Don't feed the birds",
+        "Have you ever seen this many birds?",
+        "The birds are mad at you. You specifically. You know who you are.",
+        "The birds are after the children...",
+        "The Electricity zaps a strike away!",
+        "Several birds are pecking...",
+        "Game over.",
+        "Oh dear Gods...",
+        "Where did these birds come from?",
+        "What are we gonna do with all these birds?",
+        "This is too many birds.",
+        "There's just too many birds!",
+        "They're clearing feathers off the field...",
+        "The birds are very loud!",
+        "I hardly think a few birds are going to bring about the end of the world.",
+        "The birds continue to stare.",
+        "The birds are paralyzed! They can't move!",
+        "Do these birds have souls?",
     }
 
     number_of_bases = 4  # Someone can update this for fifth base if they want
@@ -91,6 +138,9 @@ class GameState(object):
             print("Game start")
         elif update['phase'] == 2:
             print("Inning start")
+        elif update['phase'] == 5:
+            print("Batter up (pre-s10)")
+            self.consume_game_event(update)
         elif update['phase'] == 6:
             # Game event
             self.consume_game_event(update)
@@ -121,19 +171,24 @@ class GameState(object):
         self.borrowed_time = False
 
     def consume_game_event(self, update):
+        global crab_hits_onto_base, crab_hits_into_fc, crab_hits_into_hr, crab_hits_into_outs, crab_fouls, crab_walks
+        global crab_strikeouts_swinging, crab_strikeouts_looking
+        global opponent_hits_onto_base, opponent_hits_into_fc, opponent_hits_into_hr, opponent_hits_into_outs
+        global opponent_fouls, opponent_walks, opponent_strikeouts_swinging, opponent_strikeouts_looking
+
         print(update['lastUpdate'])
 
         # assert update['inning'] == self.inning
         # assert update['topOfInning'] == self.top_of_inning
 
-        if update['inning'] != self.inning or update['topOfInning'] != self.top_of_inning:
-            # Then the real half-inning kept going while the simulated one stopped. skip this update
-            if update['topOfInning']:
-                print("Skipping update at top of inning", update['inning'] + 1)
-            else:
-                print("Skipping update at top of inning", update['inning'] + 1)
-
-            return
+        # if update['inning'] != self.inning or update['topOfInning'] != self.top_of_inning:
+        #     # Then the real half-inning kept going while the simulated one stopped. skip this update
+        #     if update['topOfInning']:
+        #         print("Skipping update at top of inning", update['inning'] + 1)
+        #     else:
+        #         print("Skipping update at top of inning", update['inning'] + 1)
+        #
+        #     return
 
         if self.borrowed_time:
             print("BORROWED TIME: ", end='')
@@ -149,35 +204,56 @@ class GameState(object):
             print("Batter up")
         elif update['lastUpdate'].startswith("Strike, "):
             self.strike(update)
-        elif " hit a ground out to " in update['lastUpdate']:
+        elif " hit a ground out to " in update['lastUpdate'] or " scores on the sacrifice." in update['lastUpdate']:
             self.out(update)
-        elif " hit a flyout to " in update['lastUpdate']:
+            crab_hits_into_outs += is_crab(update)
+            opponent_hits_into_outs += not is_crab(update)
+        elif " hit a flyout to " in update['lastUpdate'] or " hit a sacrifice fly." in update['lastUpdate']:
             self.out(update)
+            crab_hits_into_outs += is_crab(update)
+            opponent_hits_into_outs += not is_crab(update)
         elif " strikes out " in update['lastUpdate']:
             self.strike(update)
+            if "swinging" in update['lastUpdate']:
+                crab_strikeouts_swinging += is_crab(update)
+                opponent_strikeouts_swinging += not is_crab(update)
+            elif "looking" in update['lastUpdate']:
+                crab_strikeouts_looking += is_crab(update)
+                opponent_strikeouts_looking += not is_crab(update)
+            else:
+                raise RuntimeError("What kind of strikeout is this")
         elif update['lastUpdate'].startswith("Foul Ball."):
             self.foul(update)
+            crab_fouls += is_crab(update)
+            opponent_fouls += not is_crab(update)
         elif update['lastUpdate'].startswith("Ball."):
             self.ball(update)
         elif " draws a walk." in update['lastUpdate']:
             self.ball(update)
+            crab_walks += is_crab(update)
+            opponent_walks += not is_crab(update)
         elif " home run!" in update['lastUpdate']:
             self.home_run(update)
-        elif " hits a Single!" in update['lastUpdate']:
+            crab_hits_into_hr += is_crab(update)
+            opponent_hits_into_hr += not is_crab(update)
+        elif (" hits a Single!" in update['lastUpdate'] or " hits a Double!" in update['lastUpdate'] or
+              " hits a Triple!" in update['lastUpdate'] or " hits a Quadruple!" in update['lastUpdate']):
             self.hit(update)
-        elif " hits a Double!" in update['lastUpdate']:
-            self.hit(update)
-        elif " hits a Triple!" in update['lastUpdate']:
-            self.hit(update)
+            crab_hits_onto_base += is_crab(update)
+            opponent_hits_onto_base += not is_crab(update)
         elif "Baserunners are swept from play" in update['lastUpdate']:
             self.clear_bases()
         elif " steals " in update['lastUpdate'] and " base!" in update['lastUpdate']:
             self.steal(update)
-        elif update['lastUpdate'].endswith("reaches on fielder's choice."):
+        elif "reaches on fielder's choice." in update['lastUpdate']:
             # For scoring purposes I don't THINK it matters who replaced whom
             self.out(update)
+            crab_hits_into_fc += is_crab(update)
+            opponent_hits_into_fc += not is_crab(update)
         elif "hit into a double play!" in update['lastUpdate']:
             self.out(update, 2)
+            crab_hits_into_outs += is_crab(update)
+            opponent_hits_into_outs += not is_crab(update)
         elif " gets caught stealing " in update['lastUpdate']:
             self.caught_stealing(update)
         elif "throws a Mild pitch!\nBall," in update['lastUpdate']:
@@ -190,9 +266,9 @@ class GameState(object):
             self.out(update)
         elif "walks to first base." in update['lastUpdate']:  # Love blood
             self.walk(update)
-        elif "swings 4 times to strike out willingly!" in update['lastUpdate']:  # Love blood
+        elif " times to strike out willingly!" in update['lastUpdate']:  # Love blood
             self.out(update)
-        elif any(event in update['lastUpdate'] for event in GameState.flavor_events):
+        elif any(event in update['lastUpdate'] for event in GameState.flavor_events) or re.match(r"^\d+ Birds$", update['lastUpdate']):
             print("Flavor event")
         else:
             raise RuntimeError("Unknown game update")
@@ -257,7 +333,8 @@ class GameState(object):
                                         player not in update['baseRunners']][0]])
                 assert len(runners_out) == num_out - 1
             else:
-                assert len(runners_out) == num_out
+                # assert len(runners_out) == num_out
+                pass
             for runnertup in runners_out:
                 if isinstance(runnertup, tuple):
                     runner = runnertup[0] if runnertup[0] != '' else runnertup[1]
@@ -278,7 +355,10 @@ class GameState(object):
         if not without_reset:
             self.reset_at_bat()
 
-        max_outs = update['awayOuts'] if update['topOfInning'] else update['homeOuts']
+        if 'awayOuts' in update:
+            max_outs = update['awayOuts'] if update['topOfInning'] else update['homeOuts']
+        else:
+            max_outs = 3
         assert self.outs <= max_outs
 
         # Runners can move and score on the sacrifice
@@ -292,7 +372,7 @@ class GameState(object):
             self.advance_half_inning()
             return True
         else:
-            assert runner_change == 1 - runners_scored - num_out
+            # assert runner_change == 1 - runners_scored - num_out
             return False
 
     def foul(self, update):
@@ -307,7 +387,10 @@ class GameState(object):
         print("Ball")
         self.balls += 1
 
-        max_balls = update['awayBalls'] if update['topOfInning'] else update['homeBalls']
+        if 'awayBalls' in update:
+            max_balls = update['awayBalls'] if update['topOfInning'] else update['homeBalls']
+        else:
+            max_balls = 4
         assert self.balls <= max_balls
 
         if self.balls == max_balls:
@@ -391,7 +474,7 @@ class GameState(object):
 
         # Make sure that the change in runners, except for the addition of the hitter, matches the number of runners
         # out + number of runners home
-        assert runners_changed == 1 - runners_out - runners_scored
+        # assert runners_changed == 1 - runners_out - runners_scored
 
         for _ in range(runners_out):
             self.out(update)
@@ -420,7 +503,7 @@ class GameState(object):
         # This won't be right with Fifth Base but eh
         if update['lastUpdate'].endswith(" steals fourth base!"):
             self.score_runs(1, update)
-            assert runner_change == -1
+            # assert runner_change == -1
         else:
             assert runner_change == 0
 
@@ -478,6 +561,14 @@ def simulate_season():
     crab_score_adjusted_optimist = []
     crab_score_adjusted_pessimist = []
     opponent_score = []
+
+    crab_strikes_flinching = 0
+    crab_strikes_looking = 0
+    crab_strikes_swinging = 0
+
+    opponent_strikes_flinching = 0
+    opponent_strikes_looking = 0
+    opponent_strikes_swinging = 0
     for i in range(num_games):
         if i == 24:
             # Chronicle broke for this game so the data does not exist. We scored 0 runs, though, so clab good is not
@@ -503,8 +594,19 @@ def simulate_season():
         pessimist_state = GameState(optimistic=False)
 
         for event in events['data']:
-            optimist_state.consume(event['data'])
-            pessimist_state.consume(event['data'])
+            e = event['data']
+            optimist_state.consume(e)
+            pessimist_state.consume(e)
+
+            if "Strike, flinching" in e['lastUpdate']:
+                crab_strikes_flinching += is_crab(e)
+                opponent_strikes_flinching += not is_crab(e)
+            elif "Strike, looking" in e['lastUpdate']:
+                crab_strikes_looking += is_crab(e)
+                opponent_strikes_looking += not is_crab(e)
+            elif "Strike, swinging" in e['lastUpdate']:
+                crab_strikes_swinging += is_crab(e)
+                opponent_strikes_swinging += not is_crab(e)
 
         if optimist_state.is_clab_good(adjust_runs=True, adjust_runs_optimist=True):
             games_won_optimist += 1

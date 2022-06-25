2745f2dfa18f4cb442c086bf0753dd2baec36859
beiju <beiju@me.com>
2022-06-25 04:36:38 -0400

    Add more info to salmon_team_favor

--- queries/salmon_team_favor.sql
+++ queries/salmon_team_favor.sql
@@ -1,8 +1,15 @@
-select tm.nickname, q.*, q.runs_lost - q.runs_opponent_lost as net_run_loss
+select tm.nickname, q.*, 
+	times_salmon_happened - times_salmon_stole_runs as times_evaded_salmon,
+	times_salmon_stole_runs / times_salmon_happened::float as salmon_hit_rate,
+	q.runs_lost - q.runs_opponent_lost as net_run_loss
 from (select 
 		tm.team_id,
 		sum(coalesce(substring(et from concat(' ([-\d.]+) of the ', tm.nickname, '''s')), '0')::numeric) as runs_lost,
-		sum(coalesce(substring(et from concat(' ([-\d.]+) of the ', op.nickname, '''s')), '0')::numeric) as runs_opponent_lost
+		sum(coalesce(substring(et from concat(' ([-\d.]+) of the ', op.nickname, '''s')), '0')::numeric) as runs_opponent_lost,
+	    count(*) as times_salmon_happened,
+	    count(*) filter (where et like concat('% of the ', tm.nickname, '''s %')) as times_salmon_stole_runs,
+	    count(*) filter (where et like concat('% of the ', op.nickname, '''s %')) as times_salmon_stole_opponent_runs,
+	    count(*) filter (where et like concat('% of the ', tm.nickname, '''s %') and et like concat('% of the ', op.nickname, '''s %')) as times_salmon_stole_from_both_teams
 	from (select unnest(event_text) as et, * from data.game_events) ge
 	left join data.teams tm on (tm.team_id=ge.batter_team_id or tm.team_id=ge.pitcher_team_id)
 		and ge.perceived_at >= tm.valid_from
@@ -14,4 +21,4 @@ from (select
 	where et like '%The Salmon swim upstream%'
 	group by tm.team_id) q
 left join data.teams tm on tm.team_id=q.team_id and tm.valid_until is null
-order by q.runs_lost - q.runs_opponent_lost desc
+order by times_salmon_happened desc
